41c688221af756448054067cf54f0304589cca54
Zelong Kuang <zelongk@wasteproducer.home.arpa>
2025-12-16 08:31:58 +1100

    Update font size on debian

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -60,9 +60,9 @@
    (add-to-list 'default-frame-alist '(width . 120)))
   
   ('gnu/linux  ; Linux (including Debian)
-   (set-face-attribute 'default nil :font "Maple Mono-12")  ; 20 * 1.5
-   (add-to-list 'default-frame-alist '(height . 40))
-   (add-to-list 'default-frame-alist '(width . 100))))
+   (set-face-attribute 'default nil :font "Maple Mono-14")  ; 20 * 1.5
+   (add-to-list 'default-frame-alist '(height . 45))
+   (add-to-list 'default-frame-alist '(width . 110))))
 
 
 (use-package diff-hl
