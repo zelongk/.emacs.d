119d18f93cb2edce1b2cad6654fa698f8c969e12
Zelong Kuang <zelongk@wasteproducer.home.arpa>
2025-12-21 10:40:03 +1100

    Update

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -2,14 +2,15 @@
 
 
 (use-package haskell-mode
-  :hook (haskell-mode . haskell-collapse-mode)
-  :hook (haskell-mode . interactive-haskell-mode)
+  :hook (haskell-mode . (haskell-collapse-mode
+                         interactive-haskell-mode
+                         haskell-doc-mode
+                         hindent-mode))
   :mode (("\\.hs\\'" . haskell-mode))
   :config
   (setq haskell-process-suggest-remove-import-lines t
         haskell-process-auto-import-loaded-modules t)
-  (add-to-list 'completion-ignored-extensions ".hi")
-  )
+  (add-to-list 'completion-ignored-extensions ".hi"))
 
 
 (provide 'init-haskell)
