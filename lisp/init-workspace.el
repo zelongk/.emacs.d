df23a601398831d04ddd63a2823a5926dffb4f80
Zelong Kuang <zelongk@wasteproducer.home.arpa>
2025-12-22 20:32:26 +1100

    Revert "Add centaur tabs"
    This reverts commit c979a97d87e275de5861ae102cd5ccd505f61b18.

--- lisp/init-workspace.el
+++ lisp/init-workspace.el
@@ -62,11 +62,4 @@
         (quit-windows-on messages-buffer-name)))
     (advice-add #'tabspaces-restore-session :after #'my-tabspaces-burry-window)))
 
-(use-package centaur-tabs
-  :demand t
-  :hook (elpaca-after-init . centaur-tabs-mode)
-  :bind
-  ("C-<prior>" . centaur-tabs-backward)
-  ("C-<next>" . centaur-tabs-forward))
-
 (provide 'init-workspace)
